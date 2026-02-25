2401f716f7b9c88813bd92f2e3271d5048ae5d1d
Sarthak <sr.sarthak.talukdar@gmail.com>
2026-02-25 17:20:58 -0500

    Added tasks boiler plate

--- memgrind.c
+++ memgrind.c
@@ -7,8 +7,29 @@
 #include "mymalloc.h"
 #endif
 
+void task1(){
+
+}
+void task2(){
+
+}
+void task3(){
+
+}
+void task4(){
+
+}
+void task5(){
+
+}
+
 int main(int argc, char* argv[]){
 
-
+    task1();
+    task2();
+    task3();
+    task4();
+    task5();
+    
     return EXIT_SUCCESS;
 }
